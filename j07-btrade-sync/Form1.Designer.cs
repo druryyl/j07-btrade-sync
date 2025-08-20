26e4db87818d4f1e9bfecebb953b8e57e5f87de4
Jude <drury_yl@yahoo.com>
2025-08-20 13:02:23 +0700

    Update sync order-item; include qty bonus dan discount2

--- j07-btrade-sync/Form1.Designer.cs
+++ j07-btrade-sync/Form1.Designer.cs
@@ -31,47 +31,92 @@
             this.SyncBrgButton = new System.Windows.Forms.Button();
             this.SyncCustomerButton = new System.Windows.Forms.Button();
             this.SyncSalesPersonBotton = new System.Windows.Forms.Button();
+            this.IncrementalDownloadOrderButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
             // 
             // SyncBrgButton
             // 
-            this.SyncBrgButton.Location = new System.Drawing.Point(23, 25);
+            this.SyncBrgButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SyncBrgButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SyncBrgButton.Location = new System.Drawing.Point(131, 12);
             this.SyncBrgButton.Name = "SyncBrgButton";
             this.SyncBrgButton.Size = new System.Drawing.Size(113, 25);
             this.SyncBrgButton.TabIndex = 0;
             this.SyncBrgButton.Text = "Sync Barang";
-            this.SyncBrgButton.UseVisualStyleBackColor = true;
+            this.SyncBrgButton.UseVisualStyleBackColor = false;
             // 
             // SyncCustomerButton
             // 
-            this.SyncCustomerButton.Location = new System.Drawing.Point(23, 56);
+            this.SyncCustomerButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SyncCustomerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SyncCustomerButton.Location = new System.Drawing.Point(250, 12);
             this.SyncCustomerButton.Name = "SyncCustomerButton";
-            this.SyncCustomerButton.Size = new System.Drawing.Size(113, 25);
+            this.SyncCustomerButton.Size = new System.Drawing.Size(102, 25);
             this.SyncCustomerButton.TabIndex = 1;
             this.SyncCustomerButton.Text = "Sync Customer";
-            this.SyncCustomerButton.UseVisualStyleBackColor = true;
+            this.SyncCustomerButton.UseVisualStyleBackColor = false;
             // 
             // SyncSalesPersonBotton
             // 
-            this.SyncSalesPersonBotton.Location = new System.Drawing.Point(23, 87);
+            this.SyncSalesPersonBotton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SyncSalesPersonBotton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SyncSalesPersonBotton.Location = new System.Drawing.Point(358, 12);
             this.SyncSalesPersonBotton.Name = "SyncSalesPersonBotton";
-            this.SyncSalesPersonBotton.Size = new System.Drawing.Size(113, 25);
+            this.SyncSalesPersonBotton.Size = new System.Drawing.Size(116, 25);
             this.SyncSalesPersonBotton.TabIndex = 2;
             this.SyncSalesPersonBotton.Text = "Sync Sales Person";
-            this.SyncSalesPersonBotton.UseVisualStyleBackColor = true;
+            this.SyncSalesPersonBotton.UseVisualStyleBackColor = false;
+            // 
+            // IncrementalDownloadOrderButton
+            // 
+            this.IncrementalDownloadOrderButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.IncrementalDownloadOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.IncrementalDownloadOrderButton.Location = new System.Drawing.Point(12, 12);
+            this.IncrementalDownloadOrderButton.Name = "IncrementalDownloadOrderButton";
+            this.IncrementalDownloadOrderButton.Size = new System.Drawing.Size(113, 25);
+            this.IncrementalDownloadOrderButton.TabIndex = 3;
+            this.IncrementalDownloadOrderButton.Text = "Download Now";
+            this.IncrementalDownloadOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogTextBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LogTextBox.Location = new System.Drawing.Point(12, 43);
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(462, 294);
+            this.LogTextBox.TabIndex = 4;
+            this.LogTextBox.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 349);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(486, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 378);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(133)))), ((int)(((byte)(162)))));
+            this.ClientSize = new System.Drawing.Size(486, 371);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.IncrementalDownloadOrderButton);
             this.Controls.Add(this.SyncSalesPersonBotton);
             this.Controls.Add(this.SyncCustomerButton);
             this.Controls.Add(this.SyncBrgButton);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BTrade Sync";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,6 +125,9 @@
         private System.Windows.Forms.Button SyncBrgButton;
         private System.Windows.Forms.Button SyncCustomerButton;
         private System.Windows.Forms.Button SyncSalesPersonBotton;
+        private System.Windows.Forms.Button IncrementalDownloadOrderButton;
+        private System.Windows.Forms.RichTextBox LogTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- j07-btrade-sync/Form1.cs
+++ j07-btrade-sync/Form1.cs
@@ -1,8 +1,12 @@
 ﻿using j07_btrade_sync.Repository;
 using j07_btrade_sync.Service;
+using Nuna.Lib.ValidationHelper;
 using System;
+using System.Drawing;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace j07_btrade_sync
 {
@@ -13,12 +17,20 @@ namespace j07_btrade_sync
         private readonly SalesPersonSyncService _salesPersonSyncService;
         private readonly KategoriSyncService _kategoriSyncService;
         private readonly WilayahSyncService _wilayahSyncService;
+        private readonly OrderIncrementalDownloadService _orderDownloadService;
 
         private readonly BrgDal _brgDal;
         private readonly CustomerDal _customerDal;
         private readonly SalesPersonDal _salesPersonDal;
         private readonly KategoriDal _kategoriDal;
         private readonly WilayahDal _wilayahDal;
+        private readonly OrderDal _orderDal;
+        private readonly OrderItemDal _orderItemDal;
+
+        private Timer timerClock = new Timer();
+        private System.Timers.Timer processingTimer;
+        private int processingIntervalMinutes = 5;
+
 
         public Form1()
         {
@@ -29,48 +41,175 @@ namespace j07_btrade_sync
             _salesPersonSyncService = new SalesPersonSyncService();
             _kategoriSyncService = new KategoriSyncService();
             _wilayahSyncService = new WilayahSyncService();
+            _orderDownloadService = new OrderIncrementalDownloadService();
 
             _brgDal = new BrgDal();
             _customerDal = new CustomerDal();
             _salesPersonDal = new SalesPersonDal();
             _kategoriDal = new KategoriDal();
             _wilayahDal = new WilayahDal();
+            _orderDal = new OrderDal();
+            _orderItemDal = new OrderItemDal();
 
             SyncBrgButton.Click += SyncBrgButton_Click;
             SyncCustomerButton.Click += SyncCustomerButton_Click;
             SyncSalesPersonBotton.Click += SyncSalesPersonButton_Click;
+            IncrementalDownloadOrderButton.Click += DownloadOrderButton_Click;
+            InitializeTimer();
+            InitializeClock();
+            LogMessage("BTrade Sync started.");
+            ProcessData();
+            var nextAuto = DateTime.Now.AddMinutes(processingIntervalMinutes);
+            LogMessage($"Next auto download start at {nextAuto:HH:mm:ss}");
+
+        }
+        public void LogMessage(string message, Color? color = null)
+        {
+            if (LogTextBox.InvokeRequired)
+            {
+                LogTextBox.Invoke(new Action(() => LogMessage(message, color)));
+                return;
+            }
+
+            color = color ?? Color.Black;
+
+            LogTextBox.SelectionStart = LogTextBox.TextLength;
+            LogTextBox.SelectionLength = 0;
+            LogTextBox.SelectionColor = color.Value;
+            LogTextBox.AppendText($"{DateTime.Now:HH:mm:ss} - {message}{Environment.NewLine}");
+            LogTextBox.SelectionColor = LogTextBox.ForeColor;
+            LogTextBox.ScrollToCaret();
+        }
+
+        private async void ProcessData()
+        {
+            try
+            {
+                LogMessage("Starting processing cycle...");
+                var today = DateTime.Now.Date;
+                var startDate = today.AddDays(-3);
+                var periode = new Periode(startDate, today);
+                var result = await _orderDownloadService.Execute(periode);
+                
+                if (result.Item1)
+                {
+                    var listOrder = result.Item3;
+                    if (listOrder != null && listOrder.Any())
+                    {
+                        foreach (var order in listOrder)
+                        {
+                            var orderDb = _orderDal.GetData(order);
+                            if (orderDb != null)
+                                continue;
+
+                            _orderDal.Insert(order);
+                            _orderItemDal.Delete(order);
+                            _orderItemDal.Insert(order.ListItems);
+                            LogMessage($"Download order {order.SalesName} - {order.CustomerName} ...", Color.Blue);
+                        }
+                        LogMessage($"Download done");
+                    }
+                    else
+                    {
+                        LogMessage("No orders found");
+                    }
+                }
+                else
+                {
+                    LogMessage($"Download failed: {result.Item2}", Color.Red);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"ERROR: {ex.Message}", Color.Red);
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            processingTimer = new System.Timers.Timer(processingIntervalMinutes * 60 * 1000);
+            processingTimer.Elapsed += ProcessingTimer_Elapsed;
+            processingTimer.AutoReset = false; // We'll manually restart after processing
+            processingTimer.Start();
+        }
+        private void InitializeClock()
+        {
+            var timeLabel = new ToolStripStatusLabel();
+            timeLabel.BackColor = Color.WhiteSmoke;
+            timeLabel.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+            timeLabel.ForeColor = Color.Gray;
+            statusStrip1.Items.Add(timeLabel);
+
+            timerClock.Interval = 1000;
+            timerClock.Tick += (s, e) => {
+                timeLabel.Text = $"Running Timer {DateTime.Now.ToString("HH:mm:ss")}";
+            };
+            timerClock.Start();
+        }
+        private async void ProcessingTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.BeginInvoke((Action)(() =>
+            {
+                //LogMessage($"Timer triggered - Starting processing at {DateTime.Now}");
+            }));
+
+            try
+            {
+                await Task.Run(() => ProcessData()); // Run processing on background thread
+            }
+            catch (Exception ex)
+            {
+                this.BeginInvoke((Action)(() =>
+                {
+                    LogMessage($"Processing error: {ex.Message}", Color.Red);
+                }));
+            }
+            finally
+            {
+                processingTimer.Start(); // Restart the timer
+                var nextAuto = DateTime.Now.AddMinutes(processingIntervalMinutes);
+                LogMessage($"Next auto download start at {nextAuto:HH:mm:ss}");
+            }
+        }
+
+        private void DownloadOrderButton_Click(object sender, EventArgs e)
+        {
+            LogMessage($"Manual download triggered", Color.Green);
+            ProcessData();
         }
 
         private void SyncCustomerButton_Click(object sender, EventArgs e)
         {
+            LogMessage("Sync Customer started...", Color.Green);
             var listCustomer = _customerDal.ListData().ToList();
             var result = _customerSyncService.SyncCustomer(listCustomer);
             result.ContinueWith(task =>
             {
                 if (task.Result.Item1)
                 {
-                    MessageBox.Show("Sync successful!");
+                    LogMessage("Done");
                 }
                 else
                 {
-                    MessageBox.Show($"Sync failed: {task.Result.Item2}");
+                    LogMessage($"Sync failed: {task.Result.Item2}", Color.Red);
                 }
             });
         }
 
         private void SyncBrgButton_Click(object sender, EventArgs e)
         {
+            LogMessage("Sync Barang started...", Color.Green);
             var listBrg = _brgDal.ListData().ToList();
             var result = _brgSyncService.SyncBrg(listBrg);
             result.ContinueWith(task =>
             {
                 if (task.Result.Item1)
                 {
-                    //MessageBox.Show("Sync successful!");
+                    LogMessage("Done");
                 }
                 else
                 {
-                    MessageBox.Show($"Sync failed: {task.Result.Item2}");
+                    LogMessage($"Sync failed: {task.Result.Item2}", Color.Red);
                     return;
                 }
             });
@@ -81,28 +220,29 @@ namespace j07_btrade_sync
             {
                 if (task.Result.Item1)
                 {
-                    MessageBox.Show("Brg-Kategori sync successful!");
+                    //MessageBox.Show("Brg-Kategori sync successful!");
                 }
                 else
                 {
-                    MessageBox.Show($"Kategori sync failed: {task.Result.Item2}");
+                    //MessageBox.Show($"Kategori sync failed: {task.Result.Item2}");
                 }
             });
 
         }
         private void SyncSalesPersonButton_Click(object sender, EventArgs e)
         {
+            LogMessage("Sync Sales started...", Color.Green);
             var listSalesPerson = _salesPersonDal.ListData().ToList();
             var result = _salesPersonSyncService.SyncSalesPerson(listSalesPerson);
             result.ContinueWith(task =>
             {
                 if (task.Result.Item1)
                 {
-                    //MessageBox.Show("Sync successful!");
+                    LogMessage("Done");
                 }
                 else
                 {
-                    MessageBox.Show($"Sync failed: {task.Result.Item2}");
+                    LogMessage($"Sync failed: {task.Result.Item2}", Color.Red);
                 }
             });
 
@@ -112,11 +252,11 @@ namespace j07_btrade_sync
             {
                 if (task.Result.Item1)
                 {
-                    MessageBox.Show("SalesPerson-Wilayah sync successful!");
+                    //MessageBox.Show("SalesPerson-Wilayah sync successful!");
                 }
                 else
                 {
-                    MessageBox.Show($"Wilayah sync failed: {task.Result.Item2}");
+                    //MessageBox.Show($"Wilayah sync failed: {task.Result.Item2}");
                 }
             });
         }
