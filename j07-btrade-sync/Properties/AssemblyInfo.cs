9f23811f2616eb8e1b0b525f8414297f8ab151da
Jude <drury_yl@yahoo.com>
2026-02-16 12:14:12 +0700

    Publish (Increase Version)

--- j07-btrade-sync/Properties/AssemblyInfo.cs
+++ j07-btrade-sync/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.0.17")]
-[assembly: AssemblyFileVersion("2.2.0.17")]
+[assembly: AssemblyVersion("2.2.0.18")]
+[assembly: AssemblyFileVersion("2.2.0.18")]
